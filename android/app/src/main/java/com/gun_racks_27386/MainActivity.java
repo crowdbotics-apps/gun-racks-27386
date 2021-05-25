a2584b8abef494d816d97848dcc4901e01905f03
Crowdbotics <team@crowdbotics.com>
2021-05-25 17:52:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gun_racks_27386/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gun_racks_27386;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gun_racks_27386";
+  }
+}
